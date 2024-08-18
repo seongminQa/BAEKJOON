f9ff040635cec78588f13170f9d962873c6ecf5f
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-08-19 01:54:33 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[][] matrix = new int[9][9];
 		int max = Integer.MIN_VALUE;
 		int row = 0;
 		int col = 0;
 		
 		for(int i=0; i<=8; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<=8; j++) {
-				matrix[i][j] = sc.nextInt();
+				matrix[i][j] = Integer.parseInt(st.nextToken());
 				if(max <= matrix[i][j]) {
 					max = matrix[i][j];
 					row = i;
@@ -22,5 +26,4 @@ public class Main {
 		System.out.println(max);
 		System.out.println((row+1) + " " + (col+1));
 	}
-
 }
