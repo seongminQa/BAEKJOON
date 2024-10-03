dce4eabc97dc74474c46ef28abdcea1a75f1487e
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-10-03 14:36:59 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.73 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(min > arr[i]) min = arr[i];
+        }
+        
+        System.out.println(min);
+        
+        int[] answer = new int[arr.length-1];
+        int index = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != min) {
+                answer[index] = arr[i];
+                ++index;
+            }
+        }
+        
+        return answer;
+    }
+}
