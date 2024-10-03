7e07407544db418e3e726f660a46f949c584e3ad
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-10-03 14:55:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 11.83 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        if(len % 2 == 0) answer = s.charAt(len/2 - 1) + "" + s.charAt(len/2) + "";
+        else answer = s.charAt(len/2) + "";
+        return answer;
+    }
+}
