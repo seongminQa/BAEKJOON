d5783abc57b97def43df1a6076dc14c78ffdb771
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-10-04 14:48:06 +0900

    [level 1] Title: 직사각형 별찍기, Time: 207.14 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // System.out.println(a + b);
+        
+        String result = "";
+        
+        for(int i=0; i<a; i++) {
+            result += "*";
+        }
+        
+        for(int i=0; i<b; i++) {
+            System.out.println(result);
+        }
+    }
+}
