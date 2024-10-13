2cf82d021c3805b75ea6ade5307a3f45b352a79d
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-10-13 21:02:01 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.76 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,24 +1,26 @@
-// '버블 정렬'을 사용하여 풀어보기
-
+// 삽입 정렬로 풀어보기
 class Solution {
     public long solution(long n) {
         String result = "";
         String[] number = String.valueOf(n).split("");
+        int len = number.length;
+        int temp = 0;
         
-        for(int i=0; i<number.length; i++) {
-            for(int j=0; j<number.length-1; j++) {
-                if(Integer.parseInt(number[j]) > Integer.parseInt(number[j+1])) {
-                    int temp = Integer.parseInt(number[j]);
-                    number[j] = number[j+1];
-                    number[j+1] = String.valueOf(temp);
+        for(int i=1; i<len; i++) {
+            // 삽입 정렬은 두번째 인덱스 부터 선택
+            temp = Integer.parseInt(number[i]);
+            for(int j=i-1; j>=0; j--) {
+                if(Integer.parseInt(number[j]) > temp) {
+                    number[j+1] = number[j];
+                    number[j] = String.valueOf(temp);
                 }
             }
         }
         
-        for(int i=0; i<number.length; i++) {
-            System.out.println("number[" + i + "] = " + number[i]);
+        for(int i=0; i<len; i++) {
             result = number[i] + result;
         }
+        
         
         return Long.parseLong(result);
     }
