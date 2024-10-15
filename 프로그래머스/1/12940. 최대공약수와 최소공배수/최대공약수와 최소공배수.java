fa5bc93d2c673680281ed87b0b23fd1799163c1e
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-10-15 17:58:26 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 15.21 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,29 +1,33 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int a = 0; // 최대공약수
-        int b = 0; // 최소공배수
-        int r = 0;
-        
-        if(n > m) {
-            a = n;
-            b = m;
-        } else {
-            a = m;
-            b = n;
+        // n을 큰 수로 두고 계산할 것이다.
+        int temp = 0;
+        if(n < m) {
+            temp = n;
+            n = m;
+            m = temp;
         }
         
+        // 유클리드 호제법을 통해 최대공약수를 구한다. (원본 n, m 보존)
+        int a = n;
+        int b = m;
+        int r = 0;
+        
+        int cnt = 0; // 반복 횟수 기록
+        
         while(true) {
+            // 유클리드 호제법 적용
             r = a % b;
             a = b;
             b = r;
-            
-            if(r == 0) break;
+            cnt++;
+            if(b == 0) break;
         }
         
-        if(a == 1) b = n*m;
-        else b = n*m/a;
+        System.out.println("반복 횟수 : " + cnt);
+        System.out.println("최대공약수 : " + a);
+        System.out.println("최소공배수 : " + (a*n/a*m/a));
         
-        int[] answer = {a, b};
-        return answer;
+        return new int[] {a, (a*n/a*m/a)};
     }
 }
