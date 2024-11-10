98a214d4501dc82376da560092acc0cb18897858
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-11 05:37:03 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.06 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,20 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public Integer[] solution(int[] numbers) {
-        HashSet<Integer> number = new HashSet<>();
+    public int[] solution(int[] numbers) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
         
         for(int i=0; i<numbers.length; i++) {
-            for(int j=0; j<numbers.length; j++){
-                if(i != j) number.add(numbers[i] + numbers[j]);
+            for(int j=0; j<numbers.length; j++) {
+                if(i != j) set.add(numbers[i] + numbers[j]);
             }
         }
-
-        Integer[] result = number.toArray(new Integer[0]);
-        System.out.println(result.toString());
         
-        int[] a = new int[0];
-        Arrays.sort(result);
+        for(int i : set) {
+            list.add(i);
+        }
+        
+        Collections.sort(list);
+        System.out.println(list.toString());
+        
+        int[] result = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
         return result;
     }
 }
