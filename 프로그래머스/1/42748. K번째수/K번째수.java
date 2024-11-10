f6e523a9e53681a58de41c7a0e564fbca21c68d7
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-11 05:14:32 +0900

    [level 1] Title: K번째수, Time: 0.25 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,20 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        List<Integer> list = new ArrayList<>();
         int[] result = new int[commands.length];
         
         for(int i=0; i<commands.length; i++) {
-            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
-            for(int j=0; j<commands[i][1] - commands[i][0] + 1; j++) {
-                temp[j] = array[commands[i][0]-1+j];
+            int start = commands[i][0]-1;
+            int end = commands[i][1]-1;
+            int idx = commands[i][2]-1;
+            for(int j=start; j<=end; j++) {
+                list.add(array[j]);
             }
-            Arrays.sort(temp);
-            result[i] = temp[commands[i][2]-1];
+            Collections.sort(list);
+            result[i] = list.get(idx);
+            list.clear();
         }
         
         return result;
