ddc9a8138895494bab1279db9ec2c00d3b915616
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-17 21:45:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.21 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,37 +1,26 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
+        String[] binary1 = new String[n];
+        String[] binary2 = new String[n];
         
         for(int i=0; i<n; i++) {
-            String a = "";
-            String b = "";
-            String c = "";
-            
-            while(arr1[i] >= 1) {
-                if(arr1[i] % 2 == 1) a = "#" + a;
-                else a = " " + a;
-                arr1[i] = arr1[i] / 2;
-            }
-            if(a.length() < n) {
-                int aLeng = a.length();
-                for(int k=0; k<n-aLeng; k++) a = " " + a;
-            } 
-            
-            while(arr2[i] >= 1) {
-                if(arr2[i] % 2 == 1) b = "#" + b;
-                if(arr2[i] % 2 == 0) b = " " + b;
-                arr2[i] = arr2[i] / 2;
-            }
-            if(b.length() < n) {
-                int bLeng = b.length();
-                for(int k=0; k<n-bLeng; k++) b = " " + b;
-            }
+            int number1 = arr1[i];
+            int number2 = arr2[i];
+            StringBuilder sb = new StringBuilder();
             
             for(int j=0; j<n; j++) {
-                if(a.charAt(j) == '#' || b.charAt(j) == '#') c += "#";
-                else c += " ";
+                int rest1 = number1 % 2;
+                int rest2 = number2 % 2;
+                number1 = number1 / 2;
+                number2 = number2 / 2;
+                if(rest1 + rest2 >= 1) {
+                    sb.insert(0, "#");
+                } else {
+                    sb.insert(0, " ");
+                }
             }
-            result[i] = c;
+            result[i] = sb.toString();
         }
         
         return result;
