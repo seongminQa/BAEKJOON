f568d59cb257490a2f184b299afc5417b857f618
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-17 22:58:47 +0900

    [level 1] Title: 명예의 전당 (1), Time: 15.70 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,30 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
+        List<Integer> list = new ArrayList<>();
         int[] result = new int[score.length];
-        int min = Integer.MAX_VALUE;
         
         for(int i=0; i<score.length; i++) {
-            if(i == 0) {
-                result[i] = score[i];
-                // int[] arrayCopy = Arrays.copyOf(score, i+1);
-                // Arrays.sort(arrayCopy);
-                // // arrayCopy = Arrays.stream(arrayCopy).distinct().toArray();
-                // System.out.println("i = " + i + ", array = " + Arrays.toString(arrayCopy));
-                // System.out.println("array의 길이 = " + arrayCopy.length);
+            if(i < k) {
+                list.add(score[i]);
+                Collections.sort(list);
+                result[i] = list.get(0);
             } else {
-                int[] arrayCopy = Arrays.copyOf(score, i+1);
-                Arrays.sort(arrayCopy);
-                // arrayCopy = Arrays.stream(arrayCopy).distinct().toArray();
-                // System.out.println("i = " + i + ", array = " + Arrays.toString(arrayCopy));
-                // System.out.println("array의 길이 = " + arrayCopy.length);
-                
-                if(arrayCopy.length < k) {
-                    result[i] = arrayCopy[0];
-                } else {
-                    result[i] = arrayCopy[arrayCopy.length-k];
-                }
+                list.add(score[i]);
+                Collections.sort(list, Collections.reverseOrder());
+                result[i] = list.get(k-1);
             }
         }
         
