9ab840349c44af33411d9dbbaddf3f5b53d01582
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-23 21:49:03 +0900

    [level 1] Title: 2016년, Time: 0.25 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,37 +1,16 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-
 class Solution {
     public String solution(int a, int b) {
-        LocalDate date = LocalDate.of(2016, a, b);
-        System.out.println(date);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        System.out.println(dayOfWeek);
-        int dayOfWeekNumber = dayOfWeek.getValue();
-        System.out.println(dayOfWeekNumber);
+        int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int totalDay = 0;
         
-        return Result(dayOfWeekNumber);
-    }
-    
-    public static String Result(int num) {
-        switch(num) {
-            case 1:
-                return "MON";
-            case 2:
-                return "TUE";
-            case 3:
-                return "WED";
-            case 4:
-                return "THU";
-            case 5:
-                return "FRI";
-            case 6:
-                return "SAT";
-            case 7:
-                return "SUN";
-            default:
-                return "Invalid Day";
+        for(int i=0; i<a-1; i++) {
+            totalDay += months[i];
         }
+        System.out.println(totalDay);
+        totalDay += b - 1;
+        System.out.println(totalDay);
+        
+        return String.valueOf(days[totalDay%7]);
     }
-    
 }
