6c7f471ec4ef51416e060f733282db3eacaa3df1
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-24 18:52:18 +0900

    [level 1] Title: 소수 만들기, Time: 10.65 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,26 +2,20 @@ class Solution {
     public int solution(int[] nums) {
         int result = 0;
         int len = nums.length;
-
-        for(int i=0; i<len-2; i++) {
-            int value = 0;
-            for(int j=1+i; j<len-1; j++) {
-                for(int k=1+j; k<len; k++){
-                    value = nums[i] + nums[j] + nums[k];
-                    // System.out.println("i = " + i + " j = " + j + " k = " + k);
-                    // System.out.println(nums[i] + "+" + nums[j] + "+" + nums[k]);
-                    // System.out.println("value = " + value);
-                    // System.out.println("소수 판별 시작");
+        
+        for(int i=0; i<len; i++) {
+            for(int j=i+1; j<len; j++) {
+                for(int k=j+1; k<len; k++) {
+                    int number = nums[i]+nums[j]+nums[k];
                     boolean check = true;
-                    for(int v=2; v<=Math.sqrt(value); v++) {
-                        // System.out.println("v = " + v + " value = " + value + " value % v = " + value % v);
-                        if(value % v == 0) check = false;
+                    for(int num=2; num<=Math.sqrt(number); num++) {
+                        if(number % num == 0) check = false;
                     }
-                    // System.out.println("check = " + check);
-                    if(check) result++;
+                    if(check) ++result;
                 }
             }
         }
+        
         return result;
     }
 }
