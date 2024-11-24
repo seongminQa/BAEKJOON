9bd56595ea9188864dc4ad512c08ca7d6bde65b9
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-11-24 18:40:03 +0900

    [level 1] Title: 과일 장수, Time: 213.84 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int result = 0;
-        int len = score.length;
-        Integer[] temp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        List<Integer> list = new ArrayList<>();
+        for(int sco : score) {
+            list.add(sco);
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        // System.out.println(list);
         
-        Arrays.sort(temp, Collections.reverseOrder());
-
-        for(int i=0; i<len/m; i++) {
-            int min = Integer.MAX_VALUE;
-            for(int j=0+(m*i); j<m+(m*i); j++) {
-                if(temp[j] < min) min = temp[j];
+        int totalPrice = 0;
+        int price = 0;
+        for(int i=0; i<score.length; i++) {
+            if((i+1) % m == 0) {
+                price = list.get(i);
+                totalPrice += price * m;
             }
-            result += 1*min*m;
         }
         
-        return result;
+        return totalPrice;
     }
 }
