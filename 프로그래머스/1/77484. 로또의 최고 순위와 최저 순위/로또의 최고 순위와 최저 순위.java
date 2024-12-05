0cb5e430976942115440346c9b38301d0bd2ab6e
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-12-05 15:39:50 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,33 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        ArrayList<Integer> minwoo = new ArrayList<>();
-        HashMap<Integer, Integer> result = new HashMap<>();
-        result.put(6, 1);
-        result.put(5, 2);
-        result.put(4, 3);
-        result.put(3, 4);
-        result.put(2, 5);
-        result.put(1, 6);
-        result.put(0, 6);
-        int cnt = 0;
+        int hit = 0;
+        int zeroCnt = 0;
         
-        for(int num : lottos) {
-            if(num != 0)
-                minwoo.add(num);
-        }
-
-        // System.out.println(minwoo);
-        // System.out.println(minwoo.size());
-        
-        for(int num : win_nums) {
-            if(minwoo.contains(num)) {
-                ++cnt;
+        for(int number : lottos) {
+            if(number == 0) {
+                ++zeroCnt;
+            } else {
+                for(int win_num : win_nums) {
+                    if(number == win_num) {
+                        ++hit;
+                        break;
+                    }
+                }
             }
         }
         
+        int max_match = 0;
+        int min_match = 0;
         
-        return new int[] {result.get(cnt+(6-minwoo.size())),result.get(cnt)};
+        if(zeroCnt == 6) {
+            max_match = 6;
+            min_match = 1;
+        } else if(hit == 0) {
+            max_match = 1;
+            min_match = 1;
+        } else {
+            max_match = hit + zeroCnt;
+            min_match = hit;
+        }
+        
+        return new int[] {7-max_match, 7-min_match};
     }
 }
